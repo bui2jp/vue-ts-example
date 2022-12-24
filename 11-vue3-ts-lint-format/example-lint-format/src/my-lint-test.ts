f5118d2c78e7f5b3bc853562396c1b7038f44fcf
takuya <>
2022-12-24 12:54:19 +0900

    test

--- 11-vue3-ts-lint-format/example-lint-format/src/my-lint-test.ts
+++ 11-vue3-ts-lint-format/example-lint-format/src/my-lint-test.ts
@@ -1,1 +1,5 @@
-console.log("start my-lint-test ...")
+console.log("start my-lint-test ...");
+console.log("start my-lint-test ...");
+
+console.log("start my-lint-test ...");
+console.log("start my-lint-test ...");
