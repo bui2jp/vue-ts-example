8831372049f63dcf11ededb1fd7e8088325ea9fa
takuya <>
2023-02-05 11:44:19 +0900

    add anchor link

---
+++ 12-vue2-pdf/README.md
@@ -0,0 +1,22 @@
+# 12-vue2-pdf (w/ typescrit)
+
+pdf 表示のサンプル
+
+## create project
+
+"Manually select features" を選択して作成する。
+
+```
+# 1. インストールされていない場合、 Vue CLI をインストールしてください
+npm install --global @vue/cli
+
+# 2. 新規のプロジェクトを作成し、続いて "Manually select features" を選択して下さい
+vue create my-project-name
+```
+
+## pdfの表示について
+public or assets にある pdf ファイルを表示する
+
+## ライブラリ
+
+
---
+++ 12-vue2-pdf/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <h1> ここにPDF を表示します</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut quod praesentium tenetur perferendis non voluptatum autem asperiores ad aspernatur deleniti veritatis sint, vel impedit eum quae enim, provident excepturi iste.</p>
+
+    <div> <a href="my-sample1.pdf"> download link 1 </a> </div>
+    <div> <a href="my-sample2.pdf"> download link 2 </a> </div>
+
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut quod praesentium tenetur perferendis non voluptatum autem asperiores ad aspernatur deleniti veritatis sint, vel impedit eum quae enim, provident excepturi iste.</p>
+    
+    <button>利用規約を確認しました。</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from './components/HelloWorld.vue';
+
+@Component({
+  components: {
+    HelloWorld,
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
