c2ae1dd3388d086557dced35c4d6c8ec97a49340
takuya <>
2023-06-26 09:33:48 +0900

    up

--- 14_vue2-i18n-store/i18n-store-example/vue.config.js
+++ 14_vue2-i18n-store/i18n-store-example/vue.config.js
@@ -1,9 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  // configureWebpack: {
-  //   devtool: 'source-map'
-  // }
+  configureWebpack: {
+    devtool: 'source-map'
+  }
 })
 // module.exports = {
 //   configureWebpack: {
