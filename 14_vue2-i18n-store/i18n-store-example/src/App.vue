6b8327c0df09b38154c5b77d7cce04f1ac2c86b7
bui2jp <chikutaku.reiwa.2020@gmail.com>
2023-07-29 09:55:03 +0900

    test2 中身変更

--- 14_vue2-i18n-store/i18n-store-example/src/App.vue
+++ 14_vue2-i18n-store/i18n-store-example/src/App.vue
@@ -47,22 +47,24 @@ export default class App extends Vue {
  * @return true if the given time is in summer time, false otherwise.
  */
 
-// public isSummerTime(utcTime:any):boolean {
-//     let dt:Date = new Date(utcTime);
-//     let y:number = dt.getFullYear();
-//     let m:string = ("00" + (dt.getMonth()+1)).slice(-2);
-//     let d:string = ("00" + dt.getDate()).slice(-2);
-//     let hh:string = ("00" + dt.getHours()).slice(-2);
-//     let mm:string = ("00" + dt.getMinutes()).slice(-2);
-//     let ss:string = ("00" + dt.getSeconds()).slice(-2);
-//     let offset:number = dt.getTimezoneOffset() / 60;
-//     let hh2:string = ("00" + (dt.getHours() - offset)).slice(-2);
-//     let dt2:Date = new Date(y + "-" + m + "-" + d + " " + hh2 + ":" + mm + ":" + ss);
-//     let dt3:Date = new Date(y + "-03-01 00:00:00");
-//     let dt4:Date = new Date(y + "-11-01 00:00:00");
-//     let isSummerTime:boolean = dt3 <= dt2 && dt2 < dt4;
-//     return isSummerTime;
-//   }
+public isSummerTime(utcTime:any):boolean {
+    console.log("utcTime=" + utcTime);
+    let dt:Date = new Date(utcTime);
+    let y:number = dt.getFullYear();
+    let m:string = ("00" + (dt.getMonth()+1)).slice(-2);
+    let d:string = ("00" + dt.getDate()).slice(-2);
+    let hh:string = ("00" + dt.getHours()).slice(-2);
+    let mm:string = ("00" + dt.getMinutes()).slice(-2);
+    let ss:string = ("00" + dt.getSeconds()).slice(-2);
+    let offset:number = dt.getTimezoneOffset() / 60;
+    let hh2:string = ("00" + (dt.getHours() - offset)).slice(-2);
+    let dt2:Date = new Date(y + "-" + m + "-" + d + " " + hh2 + ":" + mm + ":" + ss);
+    let dt3:Date = new Date(y + "-03-01 00:00:00");
+    let dt4:Date = new Date(y + "-11-01 00:00:00");
+    let isSummerTime:boolean = dt3 <= dt2 && dt2 < dt4;
+    console.log("utcTime=" + utcTime);    
+    return isSummerTime;
+  }
 
 
   // カリフォルニア州の今日の日付と時間を返す関数
