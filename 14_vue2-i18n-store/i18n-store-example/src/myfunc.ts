3bc563d7d9fbeee6bb106744162e45c7d928ced6
bui2jp <chikutaku.reiwa.2020@gmail.com>
2023-07-29 09:40:08 +0900

    test

--- 14_vue2-i18n-store/i18n-store-example/src/myfunc.ts
+++ 14_vue2-i18n-store/i18n-store-example/src/myfunc.ts
@@ -1,18 +1,18 @@
   //isSummerTime
   function isSummerTime(utcTime:any):boolean {
-    var dt:Date = new Date(utcTime);
-    var y:number = dt.getFullYear();
-    var m:string = ("00" + (dt.getMonth()+1)).slice(-2);
-    var d:string = ("00" + dt.getDate()).slice(-2);
-    var hh:string = ("00" + dt.getHours()).slice(-2);
-    var mm:string = ("00" + dt.getMinutes()).slice(-2);
-    var ss:string = ("00" + dt.getSeconds()).slice(-2);
-    var offset:number = dt.getTimezoneOffset() / 60;
-    var hh2:string = ("00" + (dt.getHours() - offset)).slice(-2);
-    var dt2:Date = new Date(y + "-" + m + "-" + d + " " + hh2 + ":" + mm + ":" + ss);
-    var dt3:Date = new Date(y + "-03-01 00:00:00");
-    var dt4:Date = new Date(y + "-11-01 00:00:00");
-    var isSummerTime:boolean = dt3 <= dt2 && dt2 < dt4;
+    let dt:Date = new Date(utcTime);
+    let y:number = dt.getFullYear();
+    let m:string = ("00" + (dt.getMonth()+1)).slice(-2);
+    let d:string = ("00" + dt.getDate()).slice(-2);
+    let hh:string = ("00" + dt.getHours()).slice(-2);
+    let mm:string = ("00" + dt.getMinutes()).slice(-2);
+    let ss:string = ("00" + dt.getSeconds()).slice(-2);
+    let offset:number = dt.getTimezoneOffset() / 60;
+    let hh2:string = ("00" + (dt.getHours() - offset)).slice(-2);
+    let dt2:Date = new Date(y + "-" + m + "-" + d + " " + hh2 + ":" + mm + ":" + ss);
+    let dt3:Date = new Date(y + "-03-01 00:00:00");
+    let dt4:Date = new Date(y + "-11-01 00:00:00");
+    let isSummerTime:boolean = dt3 <= dt2 && dt2 < dt4;
     return isSummerTime;
   }
 
