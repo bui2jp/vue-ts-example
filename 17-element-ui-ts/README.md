1df953cb581594653c63563406b0e74e0784b6ec
bui2jp <chikutaku.reiwa.2020@gmail.com>
2023-09-24 19:24:46 +0900

    up

---
+++ 17-element-ui-ts/README.md
@@ -0,0 +1,13 @@
+# 17-element-ui-ts
+
+install
+```
+npm i element-ui -S
+```
+
+setup (main.ts に以下を追加)
+```
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+```
---
+++ 17-element-ui-ts/src/components/HelloWorld.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="hello">
+    <div>
+      <ul style="overflow-x: auto; white-space: nowrap; -webkit-overflow-scrolling: touch;">
+        <li v-for="(n) in 100" :key="n"><div style="background-color:blue; height:100px; width:100px">{{n}}</div></li>
+      </ul>
+    </div>
+
+    <div>
+    <h1>{{ msg }}</h1>
+    <!-- <el-carousel :interval="3000" type="always" height="500px"> -->
+    <el-carousel :interval="3000" type="card" height="500px" autoplay="false">      
+    <el-carousel-item v-for="item in 6" :key="item">
+      <div style="background-color: red; height: 500px;">
+        aaa
+      </div>
+    </el-carousel-item>
+    </el-carousel>    
+    </div>
+
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
