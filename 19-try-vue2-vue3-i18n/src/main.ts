ae0ad7619a99184bc07a5c58724e6ca0716cd927
bui2jp <chikutaku.reiwa.2020@gmail.com>
2023-10-22 11:56:16 +0900

    add-i18n(vue2)

--- 19-try-vue2-vue3-i18n/src/main.ts
+++ 19-try-vue2-vue3-i18n/src/main.ts
@@ -1,8 +1,33 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueI18n from 'vue-i18n'
 
 Vue.config.productionTip = false
 
+const messages = {
+  en: {
+    message: {
+      hello: 'hello world'
+    }
+  },
+  ja: {
+    message: {
+      hello: 'こんにちは、世界 (これはi18nのリソースですよ)'
+    }
+  }
+}
+
+Vue.use(VueI18n)
+
+// Create VueI18n instance with options
+const i18n = new VueI18n({
+  locale: 'ja', // set locale
+  messages, // set locale messages
+})
+
+
+
 new Vue({
+  i18n,
   render: h => h(App),
 }).$mount('#app')
